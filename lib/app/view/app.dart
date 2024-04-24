14beeb2ed11a342001932616e57be356f8f7d1c3
Adil Atalay Hamamcıoğlu <38231748+recoskyler@users.noreply.github.com>
2024-04-24 13:21:46 +0300

    refactor: hide debug banner

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pomo/timer/timer.dart';
 import 'package:pomo/l10n/l10n.dart';
+import 'package:pomo/timer/timer.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,6 +12,7 @@ class App extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
       ),
+      debugShowCheckedModeBanner: false,
       localizationsDelegates: S.localizationsDelegates,
       supportedLocales: S.supportedLocales,
       home: const TimerPage(),
