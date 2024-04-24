d93493f907edfdcc67ce56eedd7fca6c343c364d
Adil Atalay Hamamcıoğlu <38231748+recoskyler@users.noreply.github.com>
2024-04-24 15:40:25 +0300

    feat: dynamic locale and theme

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pomo/l10n/l10n.dart';
-import 'package:pomo/timer/timer.dart';
+import 'package:pomo/pages/settings/cubit/settings_cubit.dart';
+import 'package:pomo/pages/settings/settings.dart';
+import 'package:pomo/pages/timer/timer.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<TimerCubit>(
+          create: (context) => TimerCubit(),
+        ),
+        BlocProvider<SettingsCubit>(
+          create: (context) => SettingsCubit()..loadSettings(),
+        ),
+      ],
+      child: BlocBuilder<SettingsCubit, SettingsState>(
+        builder: (context, state) {
+          return MaterialApp(
+            theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.redAccent,
+              ),
+            ),
+            darkTheme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.redAccent,
+                brightness: Brightness.dark,
+              ),
+            ),
+            themeMode: state.themeMode,
+            locale: state.locale,
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: S.localizationsDelegates,
+            supportedLocales: S.supportedLocales,
+            routes: {
+              '/': (context) => const TimerPage(),
+              '/settings': (context) => const SettingsPage(),
+            },
+            initialRoute: '/',
+          );
+        },
       ),
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: S.localizationsDelegates,
-      supportedLocales: S.supportedLocales,
-      home: const TimerPage(),
     );
   }
 }
