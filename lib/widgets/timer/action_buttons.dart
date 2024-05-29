7785358a9828ae69845d8c819e2a87df62abc56e
Adil Atalay Hamamcıoğlu <38231748+recoskyler@users.noreply.github.com>
2024-05-29 18:00:56 +0300

    feat: add animations

--- lib/widgets/timer/action_buttons.dart
+++ lib/widgets/timer/action_buttons.dart
@@ -14,25 +14,42 @@ class ActionButtons extends StatefulWidget {
 }
 
 class _ActionButtonsState extends State<ActionButtons>
-    with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> animation;
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late AnimationController _resetController;
+  late AnimationController _skipController;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
 
-    controller = AnimationController(
+    _controller = AnimationController(
       vsync: this,
       duration: Durations.long2,
     );
 
-    animation = Tween<double>(begin: 0, end: 1).animate(controller);
+    _resetController = AnimationController(
+      vsync: this,
+      duration: Durations.extralong2,
+      animationBehavior: AnimationBehavior.preserve,
+    );
+
+    _skipController = AnimationController(
+      vsync: this,
+      duration: Durations.short2,
+      animationBehavior: AnimationBehavior.preserve,
+    );
+
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    _controller.dispose();
+    _resetController.dispose();
+    _skipController.dispose();
+
     super.dispose();
   }
 
@@ -46,20 +63,20 @@ class _ActionButtonsState extends State<ActionButtons>
           listenWhen: (previous, current) => previous.status != current.status,
           listener: (context, state) {
             if (state.status == TimerStatus.running) {
-              controller.forward();
+              _controller.forward();
             } else {
-              controller.reverse();
+              _controller.reverse();
             }
           },
         ),
         BlocListener<TimerCubit, TimerState>(
           listenWhen: (previous, current) =>
-              previous.duration != current.duration && !controller.isAnimating,
+              previous.duration != current.duration && !_controller.isAnimating,
           listener: (context, state) {
-            if (state.status == TimerStatus.running && controller.value < 1) {
-              controller.reverse();
-            } else if (controller.value > 0) {
-              controller.forward();
+            if (state.status == TimerStatus.running && _controller.value < 1) {
+              _controller.reverse();
+            } else if (_controller.value > 0) {
+              _controller.forward();
             }
           },
         ),
@@ -78,8 +95,16 @@ class _ActionButtonsState extends State<ActionButtons>
                 tooltip: l10n.reset,
                 onPressed: state.status == TimerStatus.running
                     ? null
-                    : () => context.read<TimerCubit>().reset(),
-                icon: const Icon(Icons.restore),
+                    : () {
+                        _resetController.forward(from: 0);
+
+                        context.read<TimerCubit>().reset();
+                      },
+                icon: RotationTransition(
+                  turns:
+                      Tween<double>(begin: 0, end: 1).animate(_resetController),
+                  child: const Icon(Icons.refresh),
+                ),
               ),
               const SizedBox(width: 16),
               IconButton.filled(
@@ -95,7 +120,7 @@ class _ActionButtonsState extends State<ActionButtons>
                 },
                 icon: AnimatedIcon(
                   icon: AnimatedIcons.play_pause,
-                  progress: animation,
+                  progress: _animation,
                 ),
               ),
               const SizedBox(width: 16),
@@ -103,11 +128,23 @@ class _ActionButtonsState extends State<ActionButtons>
                 builder: (context, settingsState) {
                   return IconButton.filledTonal(
                     tooltip: l10n.skipLap,
-                    onPressed: () => context.read<TimerCubit>().lap(
+                    onPressed: () {
+                      _skipController
+                          .forward(from: 0)
+                          .then((_) => _skipController.reverse());
+
+                      context.read<TimerCubit>().lap(
                           autoAdvance: state.status == TimerStatus.running,
                           settingsState: settingsState,
-                        ),
-                    icon: const Icon(Icons.skip_next),
+                          );
+                    },
+                    icon: SlideTransition(
+                      position: Tween<Offset>(
+                        begin: Offset.zero,
+                        end: const Offset(0.15, 0),
+                      ).animate(_skipController),
+                      child: const Icon(Icons.skip_next),
+                    ),
                   );
                 },
               ),
