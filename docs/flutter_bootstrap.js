5f87ba0d44f1cf3175b314d32c58f82b782a4c05
Adil Atalay Hamamcioglu <38231748+recoskyler@users.noreply.github.com>
2024-12-20 10:29:00 +0300

    build: web

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3733105035"
+    serviceWorkerVersion: "538528972"
   }
 });
